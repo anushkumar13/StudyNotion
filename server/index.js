fb7d50ee06979feec87d88e1adb5afcc970e1418
anushkumar13 <mallanushkumar@gmail.com>
2025-03-30 16:58:24 +0530

    Fixed server port binding for Render deployment

--- server/index.js
+++ server/index.js
@@ -70,6 +70,7 @@ app.get("*", (_, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`App is running at ${PORT}`)
+  })
+  
